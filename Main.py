76dcb704e4ad4a557a92bb810eb3f8a08948096b
Nim Wijetunga <nim.wijetunga@gmail.com>
2016-10-15 17:07:48 -0400

    Add files via upload
    Updated html and python file

--- Main.py
+++ Main.py
@@ -2,10 +2,13 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route("/")
 def index():
-    return render_template("index.html")
+    #Test Case
+    return render_template("index.html", firstName = "Nim", lastName = "Person")
 
 if __name__ == "__main__":
     app.run()
-    
+
+
+
