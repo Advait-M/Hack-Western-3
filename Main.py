c8464546752276746e30ce02a52cc0c8b0d5fc89
Nim Wijetunga <nim.wijetunga@gmail.com>
2016-10-15 18:01:02 -0400

    Adding FIrebase Input

--- Main.py
+++ Main.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template
-
+import pyredb
 app = Flask(__name__)
 
 @app.route("/")
@@ -8,6 +8,9 @@ def index():
     return render_template("index.html", firstName = "Nim", lastName = "Person")
 
 if __name__ == "__main__":
+    pyredb.WaitNoMore().start()
+    data = pyredb.WaitNoMore().getAll()
+    print(data)
     app.run()
 
 
