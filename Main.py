23e210d7beef7628be974abc6cfe204e20e742e4
Nim Wijetunga <nim.wijetunga@gmail.com>
2016-10-16 10:55:37 -0400

    Added BG Colors

--- Main.py
+++ Main.py
@@ -1,16 +1,56 @@
 from flask import Flask, render_template
+import datetime 
+import random
 import pyredb
 app = Flask(__name__)
+now = datetime.datetime.now()
 
-@app.route("/")
-def index():
-    #Test Case
-    return render_template("index.html", firstName = "Nim", lastName = "Person")
 
+def hexadecimalf():
+    hexadecimal = "#"
+    sumi = 0
+    for i in range(0, 6):
+        a = random.randint(48, 70)
+        while 58 <= a <= 64:
+            a = random.randint(48,70)
+        hexadecimal += chr(a)
+        sumi += a
+    if sumi < 700:
+        return hexadecimal
+    else:
+        return hexadecimalf()
+
+# @app.route("/")
+# def index():
+data = pyredb.WaitNoMore().getAll()
+#     names = []
+#     print(data)
+#     clinicNames= []
+#     for obj1 in range(len(data)):
+#         clinicInfo = {
+#             'clinic_name' : data[obj1][4],
+#             'end_time' : data[obj1][1],
+#             'location' : data[obj1][3],
+#             'start_time': data[obj1][0],
+#             'cooLatitude':  geolocator.geocode(data[obj1][3]).latitude,
+#             'cooLongitutde': geolocator.geocode(data[obj1][3]).longitude
+#         }
+#         clinicNames.append(data[obj1][4])
+#         names.append(clinicInfo);
+#
+#     clinicNames = list(set(clinicNames))
+#     print(clinicNames)
+#     clinicNames = {}
+#     return render_template("index.html", names = names)
+
+# def waitTime(currentTime):
+#     SessionEnds =
+#     for i in range(len(data)):
+#         print(currentTime, currentTime-datetime)
+    
 if __name__ == "__main__":
-    pyredb.WaitNoMore().start()
-    data = pyredb.WaitNoMore().getAll()
     print(data)
+    pyredb.WaitNoMore().start()
     app.run()
 
 
