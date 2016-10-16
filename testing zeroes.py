281801c1b1f73dd7656840fcd3925d53904a3088
Advait Maybhate <advait.maybhate9@gmail.com>
2016-10-16 01:57:40 -0400

    Delete testing zeroes.py

--- testing zeroes.py
+++
@@ -1,8 +0,0 @@
-time = "2:0"
-def formatTime(time):
-    timesList = time.split(":")
-    hours = timesList[0].zfill(2)
-    minutes = timesList[1].zfill(2)
-    formattedTime = hours + ":" + minutes
-    return formattedTime
-formatTime(time)
